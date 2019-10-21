be324fc1af6476a8d777109a7562f6344572903a
whuan1024 <whuan1024@gmail.com>
2019-10-21 15:10:08 +0800

    Test sdk api

--- app/src/main/java/com/cloudminds/vending/VendingApplication.java
+++ app/src/main/java/com/cloudminds/vending/VendingApplication.java
@@ -16,36 +16,42 @@ public class VendingApplication extends Application {
     public void onCreate() {
         super.onCreate();
         LogUtil.setEnabled(true);
-        LogUtil.i("VendingApplication OnCreate");
+        LogUtil.i("[VendingApplication] OnCreate");
         MideaCabinetSDK.INSTANCE.init(this, new ConfigBean("1","1","316DTW","vision"));
         MideaCabinetSDK.INSTANCE.setCabinetSDKListener(new MideaCabinetSDK.SDKListener() {
             @Override
             public void onOpenSuccess() {
+                //开门成功
                 LogUtil.i("[MideaCabinetSDK] onOpenSuccess");
             }
 
             @Override
             public void onOpenTimeOut() {
+                //开门超时
                 LogUtil.i("[MideaCabinetSDK] onOpenTimeOut");
             }
 
             @Override
             public void onCloseSuccess() {
+                //关门成功
                 LogUtil.i("[MideaCabinetSDK] onCloseSuccess");
             }
 
             @Override
             public void onCloseTimeOut() {
+                //关门超时
                 LogUtil.i("[MideaCabinetSDK] onCloseTimeOut");
             }
 
             @Override
             public void onOpenInnerLock(@NotNull CabinetGridDataBean cabinetGridDataBean) {
+                //开小门成功
                 LogUtil.i("[MideaCabinetSDK] onOpenInnerLock");
             }
 
             @Override
             public void onError(@NotNull ErrorMsgBean errorMsgBean) {
+                //异常监听
                 LogUtil.e("[MideaCabinetSDK] onError: errorMsgBean: " + errorMsgBean);
             }
         });
--- app/src/main/java/com/cloudminds/vending/ui/MainActivity.java
+++ app/src/main/java/com/cloudminds/vending/ui/MainActivity.java
@@ -2,38 +2,25 @@ package com.cloudminds.vending.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cloudminds.vending.R;
+import com.cloudminds.vending.utils.LogUtil;
 import com.midea.cabinet.sdk4data.MideaCabinetSDK;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mLockState;
-    private TextView mDoorState;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
+        debugUI();
+        debugSDK();
     }
 
-    private void initView() {
-        mLockState = findViewById(R.id.lock_state);
-        mDoorState = findViewById(R.id.door_state);
-        findViewById(R.id.open_lock).setOnClickListener(v -> {
-            MideaCabinetSDK.INSTANCE.openLock();
-            mLockState.setText("锁状态：" + MideaCabinetSDK.INSTANCE.checkLockState());
-            mDoorState.setText("门状态：" + MideaCabinetSDK.INSTANCE.checkDoorState());
-        });
-        findViewById(R.id.close_lock).setOnClickListener(v -> {
-            MideaCabinetSDK.INSTANCE.closeLock();
-            mLockState.setText("锁状态：" + MideaCabinetSDK.INSTANCE.checkLockState());
-            mDoorState.setText("门状态：" + MideaCabinetSDK.INSTANCE.checkDoorState());
-        });
+    private void debugUI() {
         findViewById(R.id.detect_face).setOnClickListener(v -> {
             Intent intent = new Intent(this, VendingActivity.class);
             intent.putExtra(IFragSwitcher.TARGET_FRAG, IFragSwitcher.FragDefines.FACE_DETECT);
@@ -63,6 +50,30 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, VendingActivity.class);
             intent.putExtra(IFragSwitcher.TARGET_FRAG, IFragSwitcher.FragDefines.PAYMENT_INFO);
             startActivity(intent);
+        });
+    }
+
+    private void debugSDK() {
+        findViewById(R.id.open_lock).setOnClickListener(v ->
+                MideaCabinetSDK.INSTANCE.openLock(true));
+        findViewById(R.id.lock_state).setOnClickListener(v ->
+                Toast.makeText(this, "锁状态：" + MideaCabinetSDK.INSTANCE.checkLockState(),
+                        Toast.LENGTH_SHORT).show());
+        findViewById(R.id.door_state).setOnClickListener(v ->
+                Toast.makeText(this, "门状态：" + MideaCabinetSDK.INSTANCE.checkDoorState(),
+                        Toast.LENGTH_SHORT).show());
+        findViewById(R.id.take_picture).setOnClickListener(v -> {
+            new Thread(() -> {
+                boolean camera1OK = MideaCabinetSDK.INSTANCE.checkCamera(1);
+                boolean camera2OK = MideaCabinetSDK.INSTANCE.checkCamera(2);
+                boolean camera3OK = MideaCabinetSDK.INSTANCE.checkCamera(3);
+                boolean camera4OK = MideaCabinetSDK.INSTANCE.checkCamera(4);
+                LogUtil.i("[MainActivity] camera1OK: " + camera1OK + ", camera2OK: " + camera2OK +
+                        ", camera3OK: " + camera3OK + ", camera4OK: " + camera4OK);
+                if (camera1OK && camera2OK && camera3OK && camera4OK) {
+                    MideaCabinetSDK.INSTANCE.startTakePhotos(2);
+                }
+            }).start();
         });
     }
 }
